eca00f6ace0853677c0cd0b8bff50ae3df5e6584
Pedro Vanzella <pedro@pedrovanzella.com>
2011-03-04 12:36:08 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetPool::Application.config.secret_token = 'de7a39bf48f8fed8a76efd24f03e5b4d28e201578cb6105f1db4a188e0bcf801b3991cf4981523c7ee828d55e2ef497c780f2210584fda2398d462aae804e738'
