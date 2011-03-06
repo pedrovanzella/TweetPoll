b8e4c77bdd85d4e816fc19e166568b9d4538a4d3
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-03-06 17:34:43 -0300

    fixed oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,3 @@
-OmniAuth::Strategies::Twitter, 'MQOtmTkwmCCs5fmVMXMhg', 'A2MLsH74Zqf80nXcRR1x4JyCM0T32kKppN2z5MCA'
+Rails.application.config.middleware.use OmniAuth::Builder do  
+  provider :twitter, 'MQOtmTkwmCCs5fmVMXMhg', 'A2MLsH74Zqf80nXcRR1x4JyCM0T32kKppN2z5MCA'  
+end
