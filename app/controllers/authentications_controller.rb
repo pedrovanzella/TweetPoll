e6e0ba211a80009a3257d12a7b1e9e2f29ea867b
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-03-06 18:28:18 -0300

    needs specs and tests for authentications, but it should work now

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,7 +3,22 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-	  render :text => request.env["omniauth.auth"]["uid"]
+	  omniauth = request.env["omniauth.auth"]  
+	  authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])  
+	  if authentication  
+	    flash[:notice] = "Signed in successfully."  
+	    sign_in_and_redirect(:user, authentication.user)  
+	  elsif current_user  
+	    current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
+	    flash[:notice] = "Authentication successful."  
+	    redirect_to authentications_url  
+	  else  
+	    user = User.new  
+	    user.authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])  
+	    user.save!  
+	    flash[:notice] = "Signed in successfully."  
+	    sign_in_and_redirect(:user, user)  
+	  end
   end
 
   def destroy
--- db/migrate/20110306200315_create_authentications.rb
+++ db/migrate/20110306200315_create_authentications.rb
@@ -3,6 +3,7 @@ class CreateAuthentications < ActiveRecord::Migration
     create_table :authentications do |t|
       t.integer :user_id
 			t.string :provider
+			t.string :uid
       t.timestamps
     end
   end
