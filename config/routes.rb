f3f3b53b79d84a77833dcc70ba882c7cd33d368c
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-03-06 17:39:22 -0300

    fixed oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ TweetPoll::Application.routes.draw do
 	 match '/auth/:provider/callback' => 'authentications#create'  
 	 devise_for :users  
 	 resources :authentications  
-	 root :to => 'projects#index'
+	 root :to => 'home#index'
 end
