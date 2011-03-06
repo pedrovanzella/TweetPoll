5d60c9d7710ac80829c9e339e3a5034b115b1399
Pedro Vanzella <pedro@pedrovanzella.com>
2011-03-06 14:00:45 -0300

    Devise properly installed. Added webrat dependency.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'spork', '~> 0.9.0.rc'
   gem 'autotest-rails'
   gem 'factory_girl_rails'
+  gem 'webrat'
 end
 
 # Use unicorn as the web server
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
 
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
       response.should be_success
+    end
+
+    it "should have flash messages" do
+      get 'index'
+      response.should have_selector("p", :class => "notice")
+      response.should have_selector("p", :class => "alert")
     end
   end
 
