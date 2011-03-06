997c9f07870888c2f9606c8a9578afe496d498f0
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-03-06 16:44:53 -0300

    fixed initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-CONFIG = YAML.load_file(Rails.root.join('config', 'apikeys.yml'))[Rails.env]
+CONFIG = YAML.load_file(Rails.root.join('config', 'apikeys.yml'))
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, CONFIG[:Twitter][:ConsumerKey], CONFIG[:Twitter][:ConsumerSecret]
