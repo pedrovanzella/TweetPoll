1fc0736d35f376f813712ad4c2bbd803e4807199
Pedro Vanzella <ppvanzella@gmail.com>
2011-03-06 23:22:54 -0300

    validates presence of a title

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -9,4 +9,9 @@ describe Poll do
   it "should create a new Poll given correct parameters" do
     Poll.create!(@attr)
   end
+
+  it "should require a title" do
+    no_title_poll = Poll.new(@attr.merge( title: "" ))
+    no_title_poll.should_not be_valid
+  end
 end
