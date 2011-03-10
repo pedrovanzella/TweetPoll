b7bddc78fd61759b54c67ae00d3a14c0e7784d9c
Pedro Vanzella <ppvanzella@gmail.com>
2011-03-10 17:05:59 -0300

    fixed poll_controller_spec PollController POST 'vote' when user has not voted, should be successful test, answer should have a poll

--- spec/controllers/poll_controller_spec.rb
+++ spec/controllers/poll_controller_spec.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 
 describe PollController do
-	before(:each) do
-		@poll = Poll.create!(:title => "title")
-	end
+
+  before(:each) do
+    @poll = Poll.create!(:title => "title")
+  end
+
   describe "GET show without id" do
     it "should not be successful" do
-			
       get :show, :id => @poll.id
-			Poll.find(@poll.id)
+      Poll.find(@poll.id)
       response.should be_success
     end
   end
 
   describe "POST 'vote'" do
     it "when user has not voted, should be successful" do
-			
-			@answers = []
-			(1..3).each do |i|
-				@answers << Answer.create!(:text => i.to_s)
-			end
-			@poll.answers = @answers
-			@answers.first.vote_up(User.create(:name => 'name'))
-			response.should be_successful
+      @answers = []
+      (1..3).each do |i|
+        @answers << Answer.create!(:text => i.to_s, :poll => @poll)
+      end
+      @poll.answers = @answers
+      @answers.first.vote_up(User.create(:name => 'name'))
+      response.should be_successful
     end
-
   end
 
   it "create action should redirect when model is valid" do
@@ -33,20 +32,19 @@ describe PollController do
 
   describe "GET 'destroy'" do
     it "should be successful when user is logged in and owns poll" do
-			@request.env["devise.mapping"] = :user
-			sign_in Factory.create(:user_poll)
+      @request.env["devise.mapping"] = :user
+      sign_in Factory.create(:user_poll)
       post 'destroy', :id => 1
-			@poll.destroy
+      @poll.destroy
       response.should redirect_to(root_path)
     end
 
     it "should fail if user is logged but not owner" do
-    	@request.env["devise.mapping"] = :user
-			sign_in Factory.create(:user)
-			post 'destroy', :id => 1
-			@poll.destroy
+      @request.env["devise.mapping"] = :user
+      sign_in Factory.create(:user)
+      post 'destroy', :id => 1
+      @poll.destroy
       response.should redirect_to(root_path)
     end
   end
-
 end
